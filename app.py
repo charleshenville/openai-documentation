850b74d2867a1995399ef0e43f498719a918c455
thegooner19 <thegooner19@gmail.com>
2023-05-03 19:55:28 -0400

    Update app.py
    added openapi portion

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, jsonify
 import requests
 import base64 
+import openai
+
 
 app = Flask(__name__)
+openai.api_key = "XXX"
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if request.method == 'POST':
-        return 'Hello, World! (POST)'
-    else:
-        return 'Hello, World! (GET)'
 
-@app.route('/repositories')
+@app.route('/file')
 def get_repositories():
     # Set up the GitHub API endpoint and headers
     # url = 'https://api.github.com/user/repos'
@@ -27,7 +24,7 @@ def get_repositories():
         return "No contents"
 
     url = url_base + repo
-    headers = {'Authorization': 'token XXXX'}
+    headers = {'Authorization': 'token XXX'}
 
     # Send a GET request to the GitHub API
     response = requests.get(url, headers=headers)
@@ -41,16 +38,35 @@ def get_repositories():
 
     decoded_content = base64.b64decode(content).decode('utf-8')
 
-
     # Print the content of the file
     print(decoded_content)
 
     # Convert the response to JSON and return it
     repositories = response.json()
 
+    # generate_response(decoded_content)
 
     # return jsonify(repositories)
-    return decoded_content
+    return generate_response(decoded_content)
+
+def generate_response(input_text):
+
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=input_text,
+        temperature=0.5,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        timeout=10
+    )
+    
+    # extract the response text from the API response
+    message = response.choices[0].text.strip()
+    
+    # return the response as JSON
+    return jsonify({'message': message})
+
 
 if __name__ == '__main__':
     app.run()
